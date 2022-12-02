aedd11c0feb91237c8b959d20ff59799a5a8cbc8
Lisa <velichkoeliza@gmail.com>
2022-12-02 17:09:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int bonus = 20;
+        int miles = amount / bonus;
+
+        if (miles > 0) {
+            System.out.println(miles);
+        }
+    }
+}
